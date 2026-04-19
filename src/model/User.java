bfce2373d85642423f256316bcbdd76d63c06723
wilson palma <wilsonpsouza8@gmail.com>
2026-04-19 11:49:18 -0300

    Add User model

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,49 @@
 package model;
 
+import enums.UserType;
+
 public class User {
+    private long id;
+    private String login;
+    private String password;
+    private UserType userType;
+    private Customer customer;
+
+    public User(String login, String password, UserType userType) {
+        this.login = login;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
