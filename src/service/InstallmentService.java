25905679e4fbc30456c14225afe7fb155e2d59bf
wilson palma <wilsonpsouza8@gmail.com>
2026-04-20 23:36:47 -0300

    Add InstallmentService with hasLateInstallments method

--- src/service/InstallmentService.java
+++ src/service/InstallmentService.java
@@ -1,4 +1,30 @@
 package service;
 
+import enums.InstallmentStatus;
+import model.Customer;
+import model.Installment;
+import repository.InstallmentRepository;
+import util.ValidationUtils;
+
+import java.util.List;
+
 public class InstallmentService {
+
+    private InstallmentRepository installmentRepository;
+
+
+    public InstallmentService(InstallmentRepository installmentRepository) {
+        this.installmentRepository = installmentRepository;
+    }
+
+    public boolean hasLateInstallments(Customer customer){
+        ValidationUtils.notNull(customer, "Customer");
+        List<Installment> result = installmentRepository.findByCustomer(customer);
+        for (Installment i : result){
+            if (i.getStatus() == InstallmentStatus.LATE){
+                return true;
+            }
+        }
+        return false;
+    }
 }
