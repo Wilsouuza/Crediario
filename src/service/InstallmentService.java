fc46c5137462b73fe4c01acf56ab2c34db7e19b8
wilson palma <wilsonpsouza8@gmail.com>
2026-04-21 00:51:59 -0300

    Add InstallmentService

--- src/service/InstallmentService.java
+++ src/service/InstallmentService.java
@@ -3,9 +3,13 @@ package service;
 import enums.InstallmentStatus;
 import model.Customer;
 import model.Installment;
+import model.Purchase;
 import repository.InstallmentRepository;
 import util.ValidationUtils;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
 import java.util.List;
 
 public class InstallmentService {
@@ -15,6 +19,27 @@ public class InstallmentService {
 
     public InstallmentService(InstallmentRepository installmentRepository) {
         this.installmentRepository = installmentRepository;
+    }
+
+    public void generateInstallments(Purchase purchase){
+        BigDecimal installmentValue = purchase.getValue().divide(new BigDecimal(purchase.getQtyInstallments()),2, RoundingMode.HALF_UP);
+
+        for (int i = 0; i < purchase.getQtyInstallments(); i++) {
+            LocalDate dueDate = purchase.getDate().plusDays(30L * (i + 1));
+            Installment installment = new Installment(purchase, installmentValue,dueDate);
+            purchase.addInstallment(installment);
+            installmentRepository.save(installment);
+        }
+    }
+
+    public void updateOverdueInstallments(){
+        List<Installment> installments = installmentRepository.findAll();
+        for (Installment i : installments){
+            if (i.getStatus() == InstallmentStatus.PENDING && i.getDueDate().isBefore(LocalDate.now())){
+                i.setStatus(InstallmentStatus.LATE);
+            }
+        }
+
     }
 
     public boolean hasLateInstallments(Customer customer){
@@ -27,4 +52,17 @@ public class InstallmentService {
         }
         return false;
     }
+
+    public List<Installment> findByPurchase(Purchase purchase){
+        return installmentRepository.findByPurchase(purchase);
+    }
+
+    public List<Installment> findByCustomer(Customer customer){
+        return installmentRepository.findByCustomer(customer);
+    }
+
+    public List<Installment> findAll(){
+        return installmentRepository.findAll();
+    }
+
 }
