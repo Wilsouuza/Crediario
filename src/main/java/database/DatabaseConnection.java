dd5dff291b6f3ddadb1e7e7e977ba9c4212eb701
wilson palma <wilsonpsouza8@gmail.com>
2026-04-29 23:03:12 -0300

    Secure database credentials using properties file

--- src/main/java/database/DatabaseConnection.java
+++ src/main/java/database/DatabaseConnection.java
@@ -3,14 +3,28 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/crediario";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "postgres";
+    private static final String URL;
+    private static final String USER;
+    private static final String PASSWORD;
 
     private static Connection connection;
+
+    static {
+        try {
+            Properties props = new Properties();
+            props.load(DatabaseConnection.class.getClassLoader()
+                    .getResourceAsStream("database.properties"));
+            URL = props.getProperty("db.url");
+            USER = props.getProperty("db.user");
+            PASSWORD = props.getProperty("db.password");
+        } catch (Exception e) {
+            throw new RuntimeException("Error loading database properties: " + e.getMessage());
+        }
+    }
 
     private DatabaseConnection() {}
 
